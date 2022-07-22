2e52996c731f69217161015bc11c12b5fac2ccd6
david.issa@smartbear.com <david.issa@smartbear.com>
2022-07-22 16:42:47 -0500

    method jassar

--- src/test/java/com/tatooine/jawas/IntrusionDetectionSystemTest.java
+++ src/test/java/com/tatooine/jawas/IntrusionDetectionSystemTest.java
@@ -13,61 +13,17 @@ public class IntrusionDetectionSystemTest {
      * Rigourous Test :-)
      */
     @Test
-    public void canJediObiWanAllowed() {
+    public void jassarMethod1() {
         //updated status to fail test
         assertEquals("", ids.isAllowed("Jedi-Obi-wan"), true);
     }
-	//@Ignore
     @Test
-    public void howAboutDarthVader() {
-         //updated status to fail test
-        assertEquals("", ids.isAllowed("Darth Vader"), false);
+    public void jassarMethod2() {
+        //updated status to fail test
+        assertEquals("", ids.isAllowed("Jedi-Obi-wan"), true);
     }
 
-    @Test
-    public void pricessLeia() {
-        assertEquals("", ids.isAllowed("Darth Vader"), true);
-    }
 
-    @Test
-    public void hanSolo() {
-        assertEquals("", ids.isAllowed("Jedi-hanSolo"), false);
-    }
-
-    @Test
-    public void r2d2() {
-        assertEquals("", ids.isAllowed("Jedi-helper-R2D2"), false);
-    }
-
-    @Test
-    public void chewbacca() {
-        assertEquals("", ids.isAllowed("Jedi-friend-Chubacca"), false);
-    }
-
-    @Test
-    public void jarjar() {
-        assertEquals("", ids.isAllowed("jarjar"), true);
-    }
-
-    @Test
-    public void darthSidious() {
-        assertEquals("Darth sidious is not allowed", ids.isAllowed("darth sidious"), true);
-    }
-
-    @Test
-    public void testJediYoda() {
-        assertEquals("Yoda is not allowed", ids.isAllowed("Jedi-yoda"), false);
-    }
-
-    @Test
-    public void lukeSkywalker() {
-        assertEquals("Jedi LukeSkywalker is not allowed", ids.isAllowed("Jedi-LukeSkywalker"), false);
-    }
-
-    @Test
-    public void darthMaul() {
-        assertEquals("", ids.fromDarkSide("Darth Vader"), true);
-    }
 
 
 }
